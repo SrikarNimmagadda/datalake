d74a0cf6009a2e9573ab1b5fc13c989ef6be70df
Shane Gibbons <shanegibbons@gamestop.com>
2018-01-10 20:08:32 -0600

    Add clusterfinder

---
+++ functions/start-job-store/cluster_finder.py
@@ -0,0 +1,16 @@
+class cluster_finder(object):
+    def __init__(self, cloudFormation):
+        self.cloudformation = cloudFormation
+
+    def find_cluster(self, emrStackName):
+        response = self.cloudformation.describe_stacks(StackName=emrStackName)
+        outputs = response['Stacks'][0]['Outputs']
+
+        results = filter(
+            lambda output_item: output_item['OutputKey'] == 'ClusterId',
+            outputs)
+
+        # assumes the output exists
+        cluster_id = results[0]['OutputValue']
+
+        return cluster_id
