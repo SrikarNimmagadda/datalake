052be065f8391a818f206bc20b02a50eefb11555
Shane Gibbons <shane.gibbons@gmail.com>
2018-01-11 00:35:31 -0600

    Add docker dev environment
    The other one should be clean and exactly like the build environment. The dev one is built on the python image and should stick around--don't call docker run with --rm on it.
    Also added the run-dev-env.ps1 script to kick it off on windows. It uses a bind-style mount so that you can change code in windows and test it in the container without restarting.

--- run-dev-env.ps1
+++ run-dev-env.ps1
@@ -6,9 +6,16 @@ $lf_content = [IO.File]::ReadAllText("apphash.txt") -replace "`r`n", "`n"
 [IO.File]::WriteAllText("apphash.txt", $lf_content)
 
 # Builds and runs a docker container to emulate the CodeBuild build environment
-docker build -f docker/build/Dockerfile -t tb-app-datalake-bld .
-docker run --rm -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_DEFAULT_REGION -e AWS_DEFAULT_OUTPUT -it tb-app-datalake-bld bash
+docker build -f docker/dev/Dockerfile -t tb-app-datalake-dev .
+docker run `
+    --mount type=bind,source="$(pwd)",target=/app `
+    -e AWS_ACCESS_KEY_ID `
+    -e AWS_SECRET_ACCESS_KEY `
+    -e AWS_SESSION_TOKEN `
+    -e AWS_DEFAULT_REGION `
+    -e AWS_DEFAULT_OUTPUT `
+    -it tb-app-datalake-dev bash
 
 # since we only have a latest tag for our image, we generate some cruft when we recreate it. This line removes the cruft
 echo "Pruning old images for this application"
-docker system prune --force --filter label=application=tb-app-datalake-bld
+docker system prune --force --filter label=application=tb-app-datalake
