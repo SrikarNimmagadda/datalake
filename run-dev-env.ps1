bb0cd7b98524936c9be50b371b981cf47edee43d
Shane Gibbons <shane.gibbons@gmail.com>
2018-01-11 00:55:49 -0600

    Tweak run-dev-env script to --rm the docker run

--- run-dev-env.ps1
+++ run-dev-env.ps1
@@ -5,9 +5,10 @@ git rev-parse --short HEAD | Out-File apphash.txt -Encoding UTF8
 $lf_content = [IO.File]::ReadAllText("apphash.txt") -replace "`r`n", "`n"
 [IO.File]::WriteAllText("apphash.txt", $lf_content)
 
-# Builds and runs a docker container to emulate the CodeBuild build environment
+# Builds and runs a docker container
 docker build -f docker/dev/Dockerfile -t tb-app-datalake-dev .
 docker run `
+    --rm `
     --mount type=bind,source="$(pwd)",target=/app `
     -e AWS_ACCESS_KEY_ID `
     -e AWS_SECRET_ACCESS_KEY `
@@ -18,4 +19,4 @@ docker run `
 
 # since we only have a latest tag for our image, we generate some cruft when we recreate it. This line removes the cruft
 echo "Pruning old images for this application"
-docker system prune --force --filter label=application=tb-app-datalake
+docker system prune --force --filter label=application=tb-app-datalake-dev
