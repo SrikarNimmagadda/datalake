b41f7777da4c71c813657a6fb80d41bbd5edb2ff
Shane Gibbons <shane.gibbons@gmail.com>
2018-01-12 02:43:18 -0600

    Add makefile
    As an experiment in simplification

--- pipeline-commit.sh
+++ pipeline-commit.sh
@@ -1,35 +1,9 @@
 #!/bin/bash -x
 
-# phases:
-# install:
-#   commands:
-#pip install -r dependencies.txt
-#pip install -r dev-dependencies.txt
+make bootstrap
 
-#  pre_build:
-#    commands:
-chmod +x *.sh
+make lint
 
-./prebuild.sh
+make test
 
-#  build:
-#    commands:
-echo Build started on `date`
-echo "installing dependencies"
-pip freeze > requirements.txt
-pyb install_dependencies 
-pyb analyze
-pyb publish
-echo "executing initial build per lambda fucntion"
-pyb "pkg_extract_metadata"
-pyb "pkg_route_raw" 
-pyb "pkg_start_job_store"
-
-
-# hmm if serverless is doing the pull of dependencies on package, then the unit tests have to run against the package?
-# or do we have to pull dependencies twice? That would break the principle of "build once"
-#serverless package
-
-#  post_build:
-#    commands:
-./test.sh
+make pack-lambdas
