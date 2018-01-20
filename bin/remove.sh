68358c31a59fb38acce9c0e5e249789f7c417ac5
Shane Gibbons <shanegibbons@gamestop.com>
2018-01-19 22:18:14 -0600

    Protect removing qa, prod and master stages with --force flag on script

--- bin/remove.sh
+++ bin/remove.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
+# Removes a deployment, including its data and aws resources
+
 set -e
+
+# When calling this script while the STAGE environment variable is set to a
+# protected value, the user must pass a --force flag to override and allow
+# the uninstall since this script removes data from s3 buckets
+
+force=false
+
+
+while [[ $1 != "" ]]; do
+  case $1 in
+    --force)
+      force=true
+      ;;
+  esac
+
+  shift
+done
+
+# if --file was provided, open it for writing, else duplicate stdout
+if [[ $force = "false" && ($STAGE = "prod" || $STAGE = "qa" || $STAGE = "master") ]]; then
+    echo "WARNING! You are attempting to remove a protected environment ($STAGE). If you are sure you want to remove it, call this script with the '--force' flag."
+    exit 1
+fi
 
 # include shared functions
 . bin/shared-utility.sh
@@ -16,10 +41,7 @@ function remove {
 
   # TODO: Eventually should clean all buckets, because they'll have data
   # if we're testing.
-  # Should also create a --force flag and protect specific branches,
-  # including master, qa and prod
 
-  CODE_BUCKET=$(get_bucket CodeBucket)
   clean_bucket CodeBucket
 
   serverless remove --verbose
