14577869c23c275da13ae2c216fea6f671e06fe2
Santhoshkpcg <35591562+Santhoshkpcg@users.noreply.github.com>
2018-03-02 20:02:29 +0530

    corrected as per comments
    corrected as per comments

--- spark/EMRScripts/Facts/ATTSalesActualsCSVToParquet.py
+++ spark/EMRScripts/Facts/ATTSalesActualsCSVToParquet.py
@@ -5,11 +5,11 @@ from datetime import datetime
 from pyspark.sql.types import StructType, StringType, StructField
 from pyspark.sql.functions import lit, regexp_replace, rtrim
 
-spark = SparkSession.builder.appName("ATTSalesActuals").getOrCreate()
+spark = SparkSession.builder.appName("ATTSalesActualsCSVtoParquet").getOrCreate()
 
-AttSalesActualsOutput = sys.argv[1]
-AttSalesActualsInput = sys.argv[2]
-ATTMyResultsInp2 = sys.argv[3]
+attSalesActualsOutput = sys.argv[1]
+attSalesActualsInput = sys.argv[2]
+aTTMyResultsInp2 = sys.argv[3]
 
 #########################################################################################################
 #                                 Reading the source data files                                         #
@@ -17,15 +17,15 @@ ATTMyResultsInp2 = sys.argv[3]
 
 schema = StructType([StructField('Location', StringType(), False), StructField('Loc Id', StringType(), False), StructField('Dlr1 Code', StringType(), False), StructField('KPI', StringType(), False), StructField('KPI ID', StringType(), False), StructField('Dec 2017 Actual', StringType(), True), StructField('Dec 2017 Projected', StringType(), True), StructField('Dec 2017 Target', StringType(), True)])
 
-dfATTSalesActuals = spark.sparkContext.textFile(AttSalesActualsInput).mapPartitions(lambda partition: csv.reader([line.encode('utf-8').replace('\0', '').replace('\n', '') for line in partition], delimiter=',', quotechar='"')).filter(lambda line: ''.join(line).strip() != '' and line[5] != 'Location' and len(line[0]) != 0).toDF(schema)
+dfATTSalesActuals = spark.sparkContext.textFile(attSalesActualsInput).mapPartitions(lambda partition: csv.reader([line.encode('utf-8').replace('\0', '').replace('\n', '') for line in partition], delimiter=',', quotechar='"')).filter(lambda line: ''.join(line).strip() != '' and line[5] != 'Location' and len(line[0]) != 0).toDF(schema)
 
 dfATTSalesActuals1 = dfATTSalesActuals.filter(~(dfATTSalesActuals.Location.like('Histori%%')))
 dfATTSalesActuals2 = dfATTSalesActuals1.filter(~(dfATTSalesActuals.Location.like('AT&T MyRe%%')))
 dfATTSalesActuals3 = dfATTSalesActuals2.filter(~(dfATTSalesActuals.Location.like('AR : SPRI%%')))
 dfATTSalesActuals4 = dfATTSalesActuals3.filter(~(dfATTSalesActuals.Location.like('Generated%%')))
-dfAttSalesActualsInput = dfATTSalesActuals4.filter(~(dfATTSalesActuals.Location.like('Locat%%')))
+dfattSalesActualsInput = dfATTSalesActuals4.filter(~(dfATTSalesActuals.Location.like('Locat%%')))
 
-FinalHistDF1 = dfAttSalesActualsInput.withColumnRenamed("Location", "attlocationname").\
+FinalHistDF1 = dfattSalesActualsInput.withColumnRenamed("Location", "attlocationname").\
                                                 withColumnRenamed("Loc Id", "locid").\
                                                 withColumnRenamed("Dlr1 Code", "dealercode").\
                                                 withColumnRenamed("KPI", "kpiname").\
@@ -37,16 +37,16 @@ FinalHistDF1 = dfAttSalesActualsInput.withColumnRenamed("Location", "attlocation
 FinalHistDF1 = FinalHistDF1.withColumn("actualvalue", rtrim(regexp_replace("actual_value", '\\$|\\%', '')))
 FinalHistDF1 = FinalHistDF1.withColumn("projectedvalue", rtrim(regexp_replace("projected_value", '\\$|\\%', '')))
 
-dfATTSalesActualsRPT = spark.sparkContext.textFile(ATTMyResultsInp2).mapPartitions(lambda partition: csv.reader([line.encode('utf-8').replace('\0', '').replace('\n', '') for line in partition], delimiter=',', quotechar='"')).filter(lambda line: ''.join(line).strip() != '' and line[5] != 'Location' and len(line[0]) != 0).toDF(schema)
+dfATTSalesActualsRPT = spark.sparkContext.textFile(aTTMyResultsInp2).mapPartitions(lambda partition: csv.reader([line.encode('utf-8').replace('\0', '').replace('\n', '') for line in partition], delimiter=',', quotechar='"')).filter(lambda line: ''.join(line).strip() != '' and line[5] != 'Location' and len(line[0]) != 0).toDF(schema)
 
 
 dfATTSalesActualsRPT1 = dfATTSalesActualsRPT.filter(~(dfATTSalesActualsRPT.Location.like('Histori%%')))
 dfATTSalesActualsRPT2 = dfATTSalesActualsRPT1.filter(~(dfATTSalesActualsRPT.Location.like('AT&T MyRe%%')))
 dfATTSalesActualsRPT3 = dfATTSalesActualsRPT2.filter(~(dfATTSalesActualsRPT.Location.like('AR : SPRI%%')))
 dfATTSalesActualsRPT4 = dfATTSalesActualsRPT3.filter(~(dfATTSalesActualsRPT.Location.like('Generated%%')))
-dfAttSalesActualsInputRPT = dfATTSalesActualsRPT4.filter(~(dfATTSalesActualsRPT.Location.like('Locat%%')))
+dfattSalesActualsInputRPT = dfATTSalesActualsRPT4.filter(~(dfATTSalesActualsRPT.Location.like('Locat%%')))
 
-FinalRPTDF1 = dfAttSalesActualsInputRPT.withColumnRenamed("Location", "attlocationname2").\
+FinalRPTDF1 = dfattSalesActualsInputRPT.withColumnRenamed("Location", "attlocationname2").\
                                                 withColumnRenamed("Loc Id", "locid2").\
                                                 withColumnRenamed("Dlr1 Code", "dealercode2").\
                                                 withColumnRenamed("KPI", "kpiname2").\
@@ -73,11 +73,11 @@ FinalRPTDF = spark.sql("select attlocationname2,locid2,dealercode2,kpiname2,kpii
                        "actualvalue2,projectedvalue2,targetvalue2,reportdate2,"
                        "YEAR(FROM_UNIXTIME(UNIX_TIMESTAMP())) as year,SUBSTR(FROM_UNIXTIME(UNIX_TIMESTAMP()),6,2) as month from RPT")
 
-FinalHistDF.coalesce(1).select("*").write.mode("overwrite").parquet(AttSalesActualsOutput + '/' + 'Working1')
+FinalHistDF.coalesce(1).select("*").write.mode("overwrite").parquet(attSalesActualsOutput + '/' + 'Working1')
 
-FinalRPTDF.coalesce(1).select("*").write.mode("overwrite").parquet(AttSalesActualsOutput + '/' + 'Working2')
+FinalRPTDF.coalesce(1).select("*").write.mode("overwrite").parquet(attSalesActualsOutput + '/' + 'Working2')
 
-FinalHistDF.coalesce(1).select("*").write.mode("append").partitionBy('year', 'month').format('parquet').save(AttSalesActualsOutput)
-FinalRPTDF.coalesce(1).select("*").write.mode("append").partitionBy('year', 'month').format('parquet').save(AttSalesActualsOutput)
+FinalHistDF.coalesce(1).select("*").write.mode("append").partitionBy('year', 'month').format('parquet').save(attSalesActualsOutput)
+FinalRPTDF.coalesce(1).select("*").write.mode("append").partitionBy('year', 'month').format('parquet').save(attSalesActualsOutput)
 
 spark.stop()
