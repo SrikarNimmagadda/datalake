c598bea2cf07e3b1c087b85e71a3a8fbe2e10ccc
Santhoshkpcg <35591562+Santhoshkpcg@users.noreply.github.com>
2018-03-02 20:04:21 +0530

    corrected as per comments
    corrected as per comments

--- spark/EMRScripts/Facts/ATTSalesActualsRefined.py
+++ spark/EMRScripts/Facts/ATTSalesActualsRefined.py
@@ -1,24 +1,24 @@
 from pyspark.sql import SparkSession
 import sys
 
-ATTSalesActualOP = sys.argv[1]
-AttSalesActualsInp1 = sys.argv[2]
-StoreRefineInp = sys.argv[3]
-StoreDealerAssInp = sys.argv[4]
-ATTDealerCodeInp = sys.argv[5]
-AttSalesActualsInp2 = sys.argv[6]
+attSalesActualOP = sys.argv[1]
+attSalesActualsInp1 = sys.argv[2]
+storeRefineInp = sys.argv[3]
+storeDealerAssInp = sys.argv[4]
+attDealerCodeInp = sys.argv[5]
+attSalesActualsInp2 = sys.argv[6]
 
-spark = SparkSession.builder.appName("ATTSalesActuals").getOrCreate()
+spark = SparkSession.builder.appName("ATTSalesActualsRefined").getOrCreate()
 
 #########################################################################################################
 #                                 Read the 2 source files                                              #
 #########################################################################################################
 
-dfAttSalesActualsInput = spark.read.parquet(AttSalesActualsInp1)
-dfAttSalesActualsInputRPT = spark.read.parquet(AttSalesActualsInp2)
-dfStoreRefine = spark.read.parquet(StoreRefineInp)
-dfStoreDealerAss = spark.read.parquet(StoreDealerAssInp)
-dfATTDealer = spark.read.parquet(ATTDealerCodeInp)
+dfAttSalesActualsInput = spark.read.parquet(attSalesActualsInp1)
+dfAttSalesActualsInputRPT = spark.read.parquet(attSalesActualsInp2)
+dfStoreRefine = spark.read.parquet(storeRefineInp)
+dfStoreDealerAss = spark.read.parquet(storeDealerAssInp)
+dfATTDealer = spark.read.parquet(attDealerCodeInp)
 
 dfAttSalesActualsInput.registerTempTable("HIST")
 dfAttSalesActualsInput.printSchema()
@@ -29,8 +29,6 @@ dfAttSalesActualsInputRPT.printSchema()
 FinalATTDF = spark.sql("select a.attlocationname,a.locid,a.dealercode,a.kpiname,a.reportdate,"
                        "a.kpiid,a.actualvalue,case WHEN (a.dealercode=b.dealercode2 and a.kpiname = b.kpiname2 and a.reportdate=b.reportdate2) THEN 1 ELSE 0 END as currentkpiindicator ,"
                        "a.projectedvalue from HIST a inner join RPT b on  a.attlocationname = b.attlocationname2")
-
-FinalATTDF.show(5000, False)
 
 FinalATTDF.registerTempTable("ATTSalesActuals")
 dfStoreRefine.registerTempTable("store")
@@ -49,8 +47,6 @@ dfOutput = spark.sql("select distinct a.attlocationname,a.dealercode,a.kpiid,a.k
                      "from ATTSalesActuals a inner join storedealerass b on a.dealercode = b.DealerCode inner join"
                      " store c on b.StoreNumber = c.StoreNumber inner join ATTDealer d on a.dealercode=d.dealercode where b.AssociationType='Retail' and b.AssociationStatus='Active' and a.currentkpiindicator=1")
 
-dfOutput.show(5000, False)
-
 FinaldfOutput1 = dfOutput.dropDuplicates(['dealercode', 'kpiname', 'reportdate'])
 
 FinaldfOutput1.registerTempTable("Final")
@@ -60,8 +56,8 @@ FinaldfOutput = spark.sql("select attlocationname,dealercode,kpiname,actualvalue
                           "currentkpiindicator,YEAR(FROM_UNIXTIME(UNIX_TIMESTAMP())) as year,"
                           "SUBSTR(FROM_UNIXTIME(UNIX_TIMESTAMP()),6,2) as month from Final")
 
-FinaldfOutput.coalesce(1).select("*").write.mode("overwrite").parquet(ATTSalesActualOP + '/' + 'Working')
+FinaldfOutput.coalesce(1).select("*").write.mode("overwrite").parquet(attSalesActualOP + '/' + 'Working')
 
-FinaldfOutput.coalesce(1).select("*").write.mode("append").partitionBy('year', 'month').format('parquet').save(ATTSalesActualOP)
+FinaldfOutput.coalesce(1).select("*").write.mode("append").partitionBy('year', 'month').format('parquet').save(attSalesActualOP)
 
 spark.stop()
