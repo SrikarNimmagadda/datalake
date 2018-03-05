f05f4af9ed464830c80df9a7849f97b5484144c4
Santhoshkpcg <35591562+Santhoshkpcg@users.noreply.github.com>
2018-03-05 16:49:58 +0530

    updated as per code standard
    updated as per code standard

--- spark/EMRScripts/Facts/ATTSalesActualsRefined.py
+++ spark/EMRScripts/Facts/ATTSalesActualsRefined.py
@@ -1,63 +1,71 @@
 from pyspark.sql import SparkSession
 import sys
 
-attSalesActualOP = sys.argv[1]
-attSalesActualsInp1 = sys.argv[2]
-storeRefineInp = sys.argv[3]
-storeDealerAssInp = sys.argv[4]
-attDealerCodeInp = sys.argv[5]
-attSalesActualsInp2 = sys.argv[6]
 
-spark = SparkSession.builder.appName("ATTSalesActualsRefined").getOrCreate()
+class ATTSalesActualsRefined(object):
 
-#########################################################################################################
-#                                 Read the 2 source files                                              #
-#########################################################################################################
+    def __init__(self):
 
-dfAttSalesActualsInput = spark.read.parquet(attSalesActualsInp1)
-dfAttSalesActualsInputRPT = spark.read.parquet(attSalesActualsInp2)
-dfStoreRefine = spark.read.parquet(storeRefineInp)
-dfStoreDealerAss = spark.read.parquet(storeDealerAssInp)
-dfATTDealer = spark.read.parquet(attDealerCodeInp)
+        self.appName = self.__class__.__name__
+        self.sparkSession = SparkSession.builder.appName(self.appName).getOrCreate()
+        self.attSalesActualOP = sys.argv[1]
+        self.attSalesActualsInp1 = sys.argv[2]
+        self.storeRefineInp = sys.argv[3]
+        self.storeDealerAssInp = sys.argv[4]
+        self.attDealerCodeInp = sys.argv[5]
+        self.attSalesActualsInp2 = sys.argv[6]
 
-dfAttSalesActualsInput.registerTempTable("HIST")
-dfAttSalesActualsInput.printSchema()
+        #########################################################################################################
+        #                                 Read the 2 source files                                              #
+        #########################################################################################################
 
-dfAttSalesActualsInputRPT.registerTempTable("RPT")
-dfAttSalesActualsInputRPT.printSchema()
+    def loadParquet(self):
 
-FinalATTDF = spark.sql("select a.attlocationname,a.locid,a.dealercode,a.kpiname,a.reportdate,"
-                       "a.kpiid,a.actualvalue,case WHEN (a.dealercode=b.dealercode2 and a.kpiname = b.kpiname2 and a.reportdate=b.reportdate2) THEN 1 ELSE 0 END as currentkpiindicator ,"
-                       "a.projectedvalue from HIST a inner join RPT b on  a.attlocationname = b.attlocationname2")
+        dfAttSalesActualsInput = self.sparkSession.read.parquet(self.attSalesActualsInp1)
+        dfAttSalesActualsInputRpt = self.sparkSession.read.parquet(self.attSalesActualsInp2)
+        dfStoreRefine = self.sparkSession.read.parquet(self.storeRefineInp)
+        dfStoreDealerAss = self.sparkSession.read.parquet(self.storeDealerAssInp)
+        dfATTDealer = self.sparkSession.read.parquet(self.attDealerCodeInp)
 
-FinalATTDF.registerTempTable("ATTSalesActuals")
-dfStoreRefine.registerTempTable("store")
-dfATTDealer.registerTempTable("ATTDealer")
-dfStoreDealerAss.registerTempTable("storedealerass")
+        dfAttSalesActualsInput.registerTempTable("HIST")
 
-#########################################################################################################
-#                                 Spark Transformation begins here                                      #
-#########################################################################################################
+        dfAttSalesActualsInputRpt.registerTempTable("RPT")
+
+        finalAttDf = self.sparkSession.sql("select a.attlocationname,a.locid,a.dealercode,a.kpiname,a.reportdate,"
+                                           "a.kpiid,a.actualvalue,case WHEN (a.dealercode=b.dealercode2 and a.kpiname = b.kpiname2 and a.reportdate=b.reportdate2) THEN 1 ELSE 0 END as currentkpiindicator ,"
+                                           "a.projectedvalue from HIST a inner join RPT b on  a.attlocationname = b.attlocationname2")
+
+        finalAttDf.registerTempTable("ATTSalesActuals")
+        dfStoreRefine.registerTempTable("store")
+        dfATTDealer.registerTempTable("ATTDealer")
+        dfStoreDealerAss.registerTempTable("storedealerass")
+
+        #########################################################################################################
+        #                                 Spark Transformation begins here                                      #
+        #########################################################################################################
+
+        dfOutput = self.sparkSession.sql("select distinct a.attlocationname,a.dealercode,a.kpiid,a.kpiname,"
+                                         "a.actualvalue,a.projectedvalue,b.StoreNumber as storenumber,"
+                                         "'4' as companycode,c.SpringMarket as springmarket,c.SpringRegion as springregion,c.SpringDistrict as springdistrict,a.reportdate,"
+                                         "d.attlocationid,d.attmarket,d.attregion,c.LocationName as locationname,a.currentkpiindicator "
+                                         "from ATTSalesActuals a inner join storedealerass b on a.dealercode = b.DealerCode inner join"
+                                         " store c on b.StoreNumber = c.StoreNumber inner join ATTDealer d on a.dealercode=d.dealercode where b.AssociationType='Retail' and b.AssociationStatus='Active' and a.currentkpiindicator=1")
+
+        finalDfOutput1 = dfOutput.dropDuplicates(['dealercode', 'kpiname', 'reportdate'])
+
+        finalDfOutput1.registerTempTable("Final")
+        finalDfOutput = self.sparkSession.sql("select attlocationname,dealercode,kpiname,actualvalue,projectedvalue,"
+                                              "storenumber,companycode,springmarket,springregion,springdistrict,"
+                                              "reportdate,attlocationid,attmarket,attregion,locationname,"
+                                              "currentkpiindicator,YEAR(FROM_UNIXTIME(UNIX_TIMESTAMP())) as year,"
+                                              "SUBSTR(FROM_UNIXTIME(UNIX_TIMESTAMP()),6,2) as month from Final")
+
+        finalDfOutput.coalesce(1).select("*").write.mode("overwrite").parquet(self.attSalesActualOP + '/' + 'Working')
+
+        finalDfOutput.coalesce(1).select("*").write.mode("append").partitionBy('year', 'month').format('parquet').save(self.attSalesActualOP)
+
+        self.sparkSession.stop()
 
 
-dfOutput = spark.sql("select distinct a.attlocationname,a.dealercode,a.kpiid,a.kpiname,"
-                     "a.actualvalue,a.projectedvalue,b.StoreNumber as storenumber,"
-                     "'4' as companycode,c.SpringMarket as springmarket,c.SpringRegion as springregion,c.SpringDistrict as springdistrict,a.reportdate,"
-                     "d.attlocationid,d.attmarket,d.attregion,c.LocationName as locationname,a.currentkpiindicator "
-                     "from ATTSalesActuals a inner join storedealerass b on a.dealercode = b.DealerCode inner join"
-                     " store c on b.StoreNumber = c.StoreNumber inner join ATTDealer d on a.dealercode=d.dealercode where b.AssociationType='Retail' and b.AssociationStatus='Active' and a.currentkpiindicator=1")
-
-FinaldfOutput1 = dfOutput.dropDuplicates(['dealercode', 'kpiname', 'reportdate'])
-
-FinaldfOutput1.registerTempTable("Final")
-FinaldfOutput = spark.sql("select attlocationname,dealercode,kpiname,actualvalue,projectedvalue,"
-                          "storenumber,companycode,springmarket,springregion,springdistrict,"
-                          "reportdate,attlocationid,attmarket,attregion,locationname,"
-                          "currentkpiindicator,YEAR(FROM_UNIXTIME(UNIX_TIMESTAMP())) as year,"
-                          "SUBSTR(FROM_UNIXTIME(UNIX_TIMESTAMP()),6,2) as month from Final")
-
-FinaldfOutput.coalesce(1).select("*").write.mode("overwrite").parquet(attSalesActualOP + '/' + 'Working')
-
-FinaldfOutput.coalesce(1).select("*").write.mode("append").partitionBy('year', 'month').format('parquet').save(attSalesActualOP)
-
-spark.stop()
+if __name__ == "__main__":
+    ATTSalesActualsRefined().loadParquet()
