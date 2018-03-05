7276e3d3e10fd9374389af856321cf5a01599796
Santhoshkpcg <35591562+Santhoshkpcg@users.noreply.github.com>
2018-03-05 23:40:57 +0530

    updated as per code standard
    updated as per code standard

--- spark/EMRScripts/Facts/StoreTrafficCSVToParquet.py
+++ spark/EMRScripts/Facts/StoreTrafficCSVToParquet.py
@@ -26,39 +26,52 @@ from pyspark.sql.types import StructType, StringType, IntegerType, StructField
 from pyspark.sql.functions import lit
 import csv
 
-storeTrafficInput = sys.argv[1]
-storeTrafficOutput = sys.argv[2]
 
-spark = SparkSession.builder.\
-        appName("DimStoreTrafficCSVToParquet_CSVToParquet").getOrCreate()
+class StoreTrafficCsvToParquet(object):
 
-schema = StructType([StructField('shoppertrak_locationId', StringType(), False),
-                    StructField('storename', StringType(), True),
-                    StructField('store_number', StringType(), True),
-                    StructField('trafficdate', StringType(), True),
-                    StructField('traffictime', StringType(), True),
-                    StructField('traffic', StringType(), True),
-                    StructField('traffictype', StringType(), False)])
+        def __init__(self):
+
+                self.appName = self.__class__.__name__
+                self.sparkSession = SparkSession.builder.appName(self.appName).getOrCreate()
+                self.storeTrafficInput = sys.argv[1]
+                self.storeTrafficOutput = sys.argv[2]
+
+                #########################################################################################################
+                #                                 Reading the source data files                                         #
+                #########################################################################################################
+
+        def loadParquet(self):
+
+                schema = StructType([StructField('shoppertrak_locationId', StringType(), False),
+                                     StructField('storename', StringType(), True),
+                                     StructField('store_number', StringType(), True),
+                                     StructField('trafficdate', StringType(), True),
+                                     StructField('traffictime', StringType(), True),
+                                     StructField('traffic', StringType(), True),
+                                     StructField('traffictype', StringType(), False)])
 
 # dfStoreTraffic = spark.createDataFrame(spark.sparkContext.emptyRDD(), schema)
 
+                dfStoreTraffic = self.sparkSession.sparkContext.textFile(self.storeTrafficInput).mapPartitions(lambda partition: csv.reader([line.encode('utf-8') for line in partition], delimiter=',', quotechar='"')).toDF(schema)
 
-dfStoreTraffic = spark.sparkContext.textFile(storeTrafficInput).mapPartitions(lambda partition: csv.reader([line.encode('utf-8') for line in partition], delimiter=',', quotechar='"')).toDF(schema)
+                today = datetime.now().strftime('%m/%d/%Y')
+                dfStoreTraffic = dfStoreTraffic.withColumn('reportdate', lit(today))
 
-today = datetime.now().strftime('%m/%d/%Y')
-dfStoreTraffic = dfStoreTraffic.withColumn('reportdate', lit(today))
+                dfStoreTraffic = dfStoreTraffic.withColumn("shoppertraklocationid", dfStoreTraffic["shoppertrak_locationId"].cast(IntegerType()))
+                dfStoreTraffic = dfStoreTraffic.withColumn("storenumber", dfStoreTraffic["store_number"].cast(IntegerType()))
 
-dfStoreTraffic = dfStoreTraffic.withColumn("shoppertraklocationid", dfStoreTraffic["shoppertrak_locationId"].cast(IntegerType()))
-dfStoreTraffic = dfStoreTraffic.withColumn("storenumber", dfStoreTraffic["store_number"].cast(IntegerType()))
+                dfStoreTraffic.registerTempTable("StoreTraffic")
+                dfStoreTrafficFinal = self.sparkSession.sql("select a.reportdate,a.shoppertraklocationId,a.storename ,a.storenumber,a.trafficdate,a.traffictime,a.traffic,a.traffictype, "
+                                                            "YEAR(FROM_UNIXTIME(UNIX_TIMESTAMP())) as year,SUBSTR(FROM_UNIXTIME(UNIX_TIMESTAMP()),6,2) as month from StoreTraffic a")
 
-dfStoreTraffic.registerTempTable("StoreTraffic")
-dfStoreTrafficFinal = spark.sql("select a.reportdate,a.shoppertraklocationId,a.storename ,a.storenumber,a.trafficdate,a.traffictime,a.traffic,a.traffictype, "
-                                + "YEAR(FROM_UNIXTIME(UNIX_TIMESTAMP())) as year,SUBSTR(FROM_UNIXTIME(UNIX_TIMESTAMP()),6,2) as month from StoreTraffic a")
+                dfStoreTrafficFinal.show()
 
-dfStoreTrafficFinal.show()
+                dfStoreTrafficFinal.coalesce(1).select("*").write.mode("overwrite").partitionBy('year', 'month').parquet(self.storeTrafficOutput)
 
-dfStoreTrafficFinal.coalesce(1).select("*").write.mode("overwrite").partitionBy('year', 'month').parquet(storeTrafficOutput)
+                dfStoreTrafficFinal.coalesce(1).select("*").write.mode("overwrite").parquet(self.storeTrafficOutput + '/' + 'Working')
 
-dfStoreTrafficFinal.coalesce(1).select("*").write.mode("overwrite").parquet(storeTrafficOutput + '/' + 'Working')
+                self.sparkSession.stop()
 
-spark.stop()
+
+if __name__ == "__main__":
+        StoreTrafficCsvToParquet().loadParquet()
