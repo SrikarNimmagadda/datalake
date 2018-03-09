c208ae7180a5cc1d1f5241dd052b21a862c0a400
sarathmanginapudi <sarathmanginapudi@gamestop.com>
2018-03-09 16:55:27 +0530

    emp goals updated code

--- spark/EMRScripts/Facts/EmpGoalsRefinedToDelivery.py
+++ spark/EMRScripts/Facts/EmpGoalsRefinedToDelivery.py
@@ -14,11 +14,11 @@ dfEmpGoal.registerTempTable("empgoal")
 
 #  Spark Transformation begins here
 
-dfEmpGoal = spark.sql("select a.report_date as RPT_DT, "
-                      + "a.sourceemployeeid as SRC_EMP_ID, "
-                      + "a.kpiname as KPI_NM, a.companycd as CO_CD, "
-                      + " a.sourcesystemname as SRC_SYS_NM, "
-                      + "a.goalvalue as GOAL_VAL from empgoal a")
+dfEmpGoal = spark.sql("select a.report_date as RPT_DT, " +
+                      "a.sourceemployeeid as SRC_EMP_ID, " +
+                      "a.kpiname as KPI_NM, a.companycd as CO_CD, " +
+                      " a.sourcesystemname as SRC_SYS_NM, " +
+                      "a.goalvalue as GOAL_VAL from empgoal a")
 
 
 dfEmpGoal.coalesce(1). \
