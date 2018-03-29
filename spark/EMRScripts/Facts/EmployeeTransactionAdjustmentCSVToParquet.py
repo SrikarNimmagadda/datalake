30f190a1f1587426b9a37efc298d6863c4985ee6
Binita Das <31123488+binitadas@users.noreply.github.com>
2018-03-29 18:33:27 +0530

    emptrans

--- spark/EMRScripts/Facts/EmployeeTransactionAdjustmentCSVToParquet.py
+++ spark/EMRScripts/Facts/EmployeeTransactionAdjustmentCSVToParquet.py
@@ -1,55 +1,107 @@
 from pyspark.sql import SparkSession
 import sys
+import os
 from datetime import datetime
 from pyspark.sql.functions import lit
+import boto3
+import csv
+from urlparse import urlparse
 
-spark = SparkSession.builder.appName("EmpTransAdj").getOrCreate()
 
-EmpTransAdjOut = sys.argv[1]
-EmpTransAdjIn = sys.argv[2]
+class EmpTransAdj(object):
 
-#########################################################################################################
-#                                 Reading the source data files                                         #
-#########################################################################################################
+    def __init__(self):
 
-dfEmpTrnasAdj = spark.read.format("com.databricks.spark.csv").option("header", "true").option("treatEmptyValuesAsNulls", "true").option("inferSchema", "true").load(EmpTransAdjIn)
+        self.appName = self.__class__.__name__
+        self.sparkSession = SparkSession.builder.appName(self.appName).getOrCreate()
+        self.log4jLogger = self.sparkSession.sparkContext._jvm.org.apache.log4j
+        self.logger = self.log4jLogger.LogManager.getLogger(self.appName)
+        self.s3 = boto3.resource('s3')
+        self.EmpTransAdjOut = sys.argv[1]
+        self.EmpTransAdjIn = sys.argv[2]
 
-dfEmpTrnasAdj = dfEmpTrnasAdj.withColumnRenamed("Market", "market").\
-    withColumnRenamed("Region", "region").\
-    withColumnRenamed("District", "district").\
-    withColumnRenamed("Location", "location").\
-    withColumnRenamed("Loc", "loc").\
-    withColumnRenamed("SalesPerson", "salesperson").\
-    withColumnRenamed("SalesPersonID", "salespersonid").\
-    withColumnRenamed(" DF Accessory Adjustment ", "DFAccessoryAdjustment_wxyz").\
-    withColumnRenamed(" Feature Adjustment ", "FeatureAdjustment_wxyz").\
-    withColumnRenamed(" Incorrect ActUpg ", "IncorrectActUpg_wxyz").\
-    withColumnRenamed(" Incorrect BYOD ", "IncorrectBYOD_wxyz").\
-    withColumnRenamed(" Incorrect DF ", "IncorrectDF_wxyz").\
-    withColumnRenamed(" Incorrect Prepaid ", "IncorrectPrepaid_wxyz").\
-    withColumnRenamed(" Incorrect Transactions ", "IncorrectTransactions_wxyz").\
-    withColumnRenamed(" DirecTV NOW Adjustment ", "DirecTVNOWAdjustment_wxyz").\
-    withColumnRenamed(" TV Disputes ", "TVDisputes_wxyz").\
-    withColumnRenamed(" TV Extras Adjustment ", "TVExtrasAdjustment_wxyz").\
-    withColumnRenamed(" Wired Disputes ", "WiredDisputes_wxyz").\
-    withColumnRenamed(" Wired Extras Adjustment ", "WiredExtrasAdjustment_wxyz").\
-    withColumnRenamed(" Deposit Adjustment ", "DepositAdjustment_wxyz").\
-    withColumnRenamed(" Total Adjustment ", "TotalAdjustment_wxyz")
+    def searchFile(self, strS3url):
 
-today = datetime.now().strftime('%m/%d/%Y')
-dfEmpTrnasAdj = dfEmpTrnasAdj.withColumn('reportdate', lit(today))
+        bucketWithPath = urlparse(strS3url)
+        bucket = bucketWithPath.netloc
+        path = bucketWithPath.path.lstrip('/')
+        mybucket = self.s3.Bucket(bucket)
+        objs = mybucket.objects.filter(Prefix=path)
+        file = ''
+        body = ''
+        header = ''
+        for s3Object in objs:
+            file = "s3://" + bucket + "/" + s3Object.key
+            body = s3Object.get()['Body'].read()
+        for i, line in enumerate(csv.reader(body.splitlines(), delimiter=',', quotechar='"')):
+            if i == 1:
+                header = line
 
-dfEmpTrnasAdj = dfEmpTrnasAdj.registerTempTable("EmpTransAdj")
+        return file, header
 
-FinalDF = spark.sql("select market,region,district,location,loc,salesperson,salespersonid,DFAccessoryAdjustment_wxyz,"
-                    "FeatureAdjustment_wxyz,IncorrectActUpg_wxyz,IncorrectBYOD_wxyz,IncorrectDF_wxyz,"
-                    "IncorrectPrepaid_wxyz,IncorrectTransactions_wxyz,DirecTVNOWAdjustment_wxyz,"
-                    "TVDisputes_wxyz,TVExtrasAdjustment_wxyz,WiredDisputes_wxyz,"
-                    "WiredExtrasAdjustment_wxyz,DepositAdjustment_wxyz,TotalAdjustment_wxyz,reportdate,"
-                    "YEAR(FROM_UNIXTIME(UNIX_TIMESTAMP())) as year,SUBSTR(FROM_UNIXTIME(UNIX_TIMESTAMP()),6,2) as month from EmpTransAdj")
+    def loadParquet(self):
 
-FinalDF.coalesce(1).select("*").write.mode("append").partitionBy('year', 'month').format('parquet').save(EmpTransAdjOut)
+        dfEmpTrnasAdj = self.sparkSession.read.format("com.databricks.spark.csv").\
+            option("header", "true").\
+            option("treatEmptyValuesAsNulls", "true").\
+            option("inferSchema", "true").\
+            load(self.EmpTransAdjIn)
 
-FinalDF.coalesce(1).select("*").write.mode("overwrite").parquet(EmpTransAdjOut + '/' + 'Working')
+        dfEmpTrnasAdj = dfEmpTrnasAdj.withColumnRenamed("Market", "market").\
+            withColumnRenamed("Region", "region").\
+            withColumnRenamed("District", "district").\
+            withColumnRenamed("Location", "location").\
+            withColumnRenamed("Loc", "loc").\
+            withColumnRenamed("SalesPerson", "salesperson").\
+            withColumnRenamed("SalesPersonID", "salespersonid").\
+            withColumnRenamed(" DF Accessory Adjustment ", "DFAccessoryAdjustment_wxyz").\
+            withColumnRenamed(" Feature Adjustment ", "FeatureAdjustment_wxyz").\
+            withColumnRenamed(" Incorrect ActUpg ", "IncorrectActUpg_wxyz").\
+            withColumnRenamed(" Incorrect BYOD ", "IncorrectBYOD_wxyz").\
+            withColumnRenamed(" Incorrect DF ", "IncorrectDF_wxyz").\
+            withColumnRenamed(" Incorrect Plan ", "IncorrectPlan_wxyz").\
+            withColumnRenamed("Feature Disputes", "FeatureDisputes_wxyz").\
+            withColumnRenamed(" Incorrect Prepaid ", "IncorrectPrepaid_wxyz").\
+            withColumnRenamed(" Incorrect Transactions ", "IncorrectTransactions_wxyz").\
+            withColumnRenamed(" DirecTV NOW Adjustment ", "DirecTVNOWAdjustment_wxyz").\
+            withColumnRenamed(" TV Disputes ", "TVDisputes_wxyz").\
+            withColumnRenamed(" TV Extras Adjustment ", "TVExtrasAdjustment_wxyz").\
+            withColumnRenamed(" Wired Disputes ", "WiredDisputes_wxyz").\
+            withColumnRenamed(" Wired Extras Adjustment ", "WiredExtrasAdjustment_wxyz").\
+            withColumnRenamed(" Deposit Adjustment ", "DepositAdjustment_wxyz").\
+            withColumnRenamed(" Total Adjustment ", "TotalAdjustment_wxyz")
 
-spark.stop()
+        file, fileHeader = self.searchFile(self.EmpTransAdjIn)
+        fileName, fileExtension = os.path.splitext(os.path.basename(file))
+        fields = fileName.split('\\')
+        newformat = ''
+        if len(fields) > 0:
+            filenamefield = fields[len(fields) - 1].split('_')
+            if len(filenamefield) > 0:
+                self.logger.info("###############################")
+                self.logger.info(filenamefield)
+                oldformat1 = filenamefield[1]
+                self.logger.info(oldformat1)
+                oldformat = oldformat1.replace('TransAdjEMP', '')
+                newformat = datetime.strptime(str(oldformat), '%Y%m%d').strftime('%m/%d/%Y')
+
+        dfEmpTrnasAdj = dfEmpTrnasAdj.withColumn('reportdate', lit(newformat))
+
+        dfEmpTrnasAdj.registerTempTable("EmpTransAdj")
+
+        FinalDF = self.sparkSession.sql("select market,region,district,location,loc,salesperson,salespersonid,DFAccessoryAdjustment_wxyz,"
+                                        "FeatureAdjustment_wxyz,IncorrectActUpg_wxyz,IncorrectBYOD_wxyz,IncorrectDF_wxyz,"
+                                        "IncorrectPrepaid_wxyz,IncorrectTransactions_wxyz,DirecTVNOWAdjustment_wxyz,FeatureDisputes_wxyz,"
+                                        "TVDisputes_wxyz,TVExtrasAdjustment_wxyz,WiredDisputes_wxyz,IncorrectPlan_wxyz,"
+                                        "WiredExtrasAdjustment_wxyz,DepositAdjustment_wxyz,TotalAdjustment_wxyz,reportdate,"
+                                        "YEAR(FROM_UNIXTIME(UNIX_TIMESTAMP())) as year,SUBSTR(FROM_UNIXTIME(UNIX_TIMESTAMP()),6,2) as month from EmpTransAdj")
+
+        FinalDF.coalesce(1).select("*").write.mode("append").partitionBy('year', 'month').format('parquet').save(self.EmpTransAdjOut)
+
+        FinalDF.coalesce(1).select("*").write.mode("overwrite").parquet(self.EmpTransAdjOut + '/' + 'Working')
+
+        self.sparkSession.stop()
+
+
+if __name__ == "__main__":
+    EmpTransAdj().loadParquet()
--- spark/EMRScripts/Facts/EmployeeTransactionAdjustmentRefined.py
+++ spark/EMRScripts/Facts/EmployeeTransactionAdjustmentRefined.py
@@ -1,48 +1,63 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
 import sys
-
-spark = SparkSession.builder.appName("StoreTransactionAdjustments").getOrCreate()
-
-EmpTransAdjOut = sys.argv[1]
-EmpTransAdjIn = sys.argv[2]
-EmployeeIn = sys.argv[3]
-
-dfEmpTransAdj3 = spark.read.parquet(EmpTransAdjIn)
-dfEmployee = spark.read.parquet(EmployeeIn)
-
-dfEmpTransAdj2 = dfEmpTransAdj3.drop('year')
-dfEmpTransAdj = dfEmpTransAdj2.drop('month')
+from pyspark.sql.types import DecimalType
+from pyspark.sql.functions import col
 
 
-def rowExpander(row):
-    rowDict = row.asDict()
-    MarketVal = rowDict.pop('market')
-    RegionVal = rowDict.pop('region')
-    DistrictVal = rowDict.pop('district')
-    LocationVal = rowDict.pop('location')
-    reportdateVal = rowDict.pop('reportdate')
-    S1Val = rowDict.pop('salesperson')
-    S2Val = rowDict.pop('salespersonid')
-    LocVal = rowDict.pop('loc')
-    for k in rowDict:
-        a = k.split("_")
-        yield Row(**{'market': MarketVal, 'reportdate': reportdateVal, 'region': RegionVal, 'district': DistrictVal, 'location': LocationVal, 'loc': LocVal, 'salesperson': S1Val, 'salespersonid': S2Val, 'adjustmenttype': a[0], 'adjustmentamount': row[k]})
+class EmpTransAdj(object):
+
+    def __init__(self):
+        self.appName = self.__class__.__name__
+        self.sparkSession = SparkSession.builder.appName(self.appName).getOrCreate()
+        self.log4jLogger = self.sparkSession.sparkContext._jvm.org.apache.log4j
+        self.logger = self.log4jLogger.LogManager.getLogger(self.appName)
+        self.EmpTransAdjOut = sys.argv[1]
+        self.EmpTransAdjIn = sys.argv[2]
+        self.EmployeeIn = sys.argv[3]
+
+    def loadParquet(self):
+
+        dfEmpTransAdj3 = self.sparkSession.read.parquet(self.EmpTransAdjIn)
+        dfEmployee = self.sparkSession.read.parquet(self.EmployeeIn)
+
+        dfEmpTransAdj2 = dfEmpTransAdj3.drop('year')
+        dfEmpTransAdj = dfEmpTransAdj2.drop('month')
+
+        def rowExpander(row):
+            rowDict = row.asDict()
+            MarketVal = rowDict.pop('market')
+            RegionVal = rowDict.pop('region')
+            DistrictVal = rowDict.pop('district')
+            LocationVal = rowDict.pop('location')
+            reportdateVal = rowDict.pop('reportdate')
+            S1Val = rowDict.pop('salesperson')
+            S2Val = rowDict.pop('salespersonid')
+            LocVal = rowDict.pop('loc')
+            for k in rowDict:
+                a = k.split("_")
+                yield Row(**{'market': MarketVal, 'reportdate': reportdateVal, 'region': RegionVal, 'district': DistrictVal, 'location': LocationVal, 'loc': LocVal, 'salesperson': S1Val, 'salespersonid': S2Val, 'adjustmenttype': a[0], 'adjustmentamount': row[k]})
+
+        FinalDF1 = self.sparkSession.createDataFrame(dfEmpTransAdj.rdd.flatMap(rowExpander))
+        FinalDF1.registerTempTable("EmpTransAdj1")
+
+        FinalDF_EmpTransAdj1 = self.sparkSession.sql("select market as springmarket, region as springregion, district as springdistrict, salesperson as employeename, salespersonid, reportdate, adjustmenttype, adjustmentamount from EmpTransAdj1")
+
+        dfEmployee.registerTempTable("Employee")
+
+        FinalDF_EmpTransAdj1.registerTempTable("EmpTransAdj")
+
+        FinalDF = self.sparkSession.sql("select a.springmarket, a.springregion, a.springdistrict, a.employeename, a.reportdate, a.adjustmenttype, a.adjustmentamount as adjustmentamount1, b.sourceemployeeid, 'TBSFTP' as sourcesystemname, '4' as companycd, YEAR(FROM_UNIXTIME(UNIX_TIMESTAMP())) as year, SUBSTR(FROM_UNIXTIME(UNIX_TIMESTAMP()),6,2) as month from EmpTransAdj a inner join Employee b on a.salespersonid=b.workdayid")
+
+        finalDfChangedType = FinalDF.withColumn("adjustmentamount", FinalDF["adjustmentamount1"].cast(DecimalType()))
+
+        finalDfChangedType.drop('adjustmentamount1')
+        finalDfChangedType = finalDfChangedType.where(col("sourceemployeeid").isNotNull())
+        finalDfChangedType.coalesce(1).select("*").write.mode("overwrite").parquet(self.EmpTransAdjOut + '/' + 'Working')
+        finalDfChangedType.coalesce(1).select("*").write.mode("append").parquet(self.EmpTransAdjOut + '/' + 'Previous')
+        finalDfChangedType.coalesce(1).select("*").write.mode("append").partitionBy('year', 'month').format('parquet').save(self.EmpTransAdjOut)
+        self.sparkSession.stop()
 
 
-FinalDF1 = spark.createDataFrame(dfEmpTransAdj.rdd.flatMap(rowExpander))
-FinalDF1.registerTempTable("EmpTransAdj1")
-
-FinalDF_EmpTransAdj1 = spark.sql("select market as springmarket,region as springregion,district as springdistrict,salesperson as employeename,reportdate,adjustmenttype,adjustmentamount from EmpTransAdj1")
-
-dfEmployee.registerTempTable("Employee")
-
-FinalDF_EmpTransAdj1.registerTempTable("EmpTransAdj")
-FinalDF = spark.sql(" select a.springmarket,a.springregion,a.springdistrict,b.name as employeename,"
-                    "a.reportdate,a.adjustmenttype,a.adjustmentamount,b.sourceemployeeid,"
-                    "'TBSFTP' as sourcesystemname,'4' as companycd,YEAR(FROM_UNIXTIME(UNIX_TIMESTAMP())) as year,"
-                    "SUBSTR(FROM_UNIXTIME(UNIX_TIMESTAMP()),6,2) as month from EmpTransAdj a inner join Employee b on a.employeename=b.name")
-
-FinalDF.coalesce(1).select("*").write.mode("overwrite").parquet(EmpTransAdjOut + '/' + 'Working')
-FinalDF.coalesce(1).select("*").write.mode("append").partitionBy('year', 'month').format('parquet').save(EmpTransAdjOut)
-spark.stop()
+if __name__ == "__main__":
+    EmpTransAdj().loadParquet()
